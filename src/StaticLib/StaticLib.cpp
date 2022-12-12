fac6d694db5893ddb1215fbbc7e66243ac1379e3
yuhi610 <92073990+Matuyuhi@users.noreply.github.com>
2022-12-12 19:02:58 +0900

    Update StaticLib.cpp

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,20 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (!begin || !end || end -begin < 0)return false;
+	if (end - begin < 2)return true;
+	int changef = 1;
+	while (changef) {
+		changef = 0;
+		for (int c_num = 0; begin+c_num != end-1; c_num++) {
+
+			if (begin[c_num].key > begin[c_num+1].key) {
+				item temp = begin[c_num];
+				begin[c_num] = begin[c_num+1];
+				begin[c_num + 1] = temp;
+				changef = 1;
+			}
+		}
+	}
+	return true;
 }

c8059101149b7daa3d1c83a81ace2226c4c3af7e
Nyasa0405 <105965435+Nyasa0405@users.noreply.github.com>
2022-12-13 22:10:37 +0900

    テストが通るように修正しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,26 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (!begin || !end)return false;
+	if (end < begin)return false;
+
+	int Item_num = end-begin;
+	for (int i = 0; i < Item_num-1;i++)
+	{
+		int Change_Flag = 0;
+		for (int j = Item_num - 2; j >= i; j--)
+		{
+			if (begin[j].key > begin[j + 1].key)
+			{
+
+				item Temp_Word = begin[j];
+				begin[j] = begin[j + 1];
+				begin[j + 1] = Temp_Word;
+				Change_Flag = 1;
+			}
+				if(!Change_Flag)break;
+		}
+	}
+
+	return true;
 }

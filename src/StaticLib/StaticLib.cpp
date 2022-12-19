040b35b9db0c28e26eca42575942bdd7b5409181
Hayato222 <g2227070@st.t-kougei.ac.jp>
2022-12-19 14:24:50 +0900

    テスト若干通りました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,25 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (begin == NULL || end == NULL)
+		return false;
+	if (end < begin)
+		return false;
+
+	int temp;
+
+	/*for (auto i = begin; i != end; i++)
+	{
+		for(auto j = end - 1; j !=i; i--)
+		{
+			auto temp = j;
+			if (temp < j)
+			{
+				temp = j;
+				j = j + 1;
+				//1 + j = temp;
+			}
+		}
+	}*/
+	
 }

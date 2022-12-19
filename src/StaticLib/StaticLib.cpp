3ee6f1d378c868f8c96ef81b48bdcd72c3b618ae
Mocha0924 <108125975+Mocha0924@users.noreply.github.com>
2022-12-19 18:31:08 +0900

    コミットできました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,26 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (begin == NULL || end == NULL || begin > end)
+		return false;
+	if (begin+1 == end)
+		return true;
+	item save;
+	int notfinish;
+	int count = 0;
+	for (notfinish = 0; notfinish < (end - begin)-1; notfinish++) {
+		
+		for (int i = 0; i < (end - begin) -1-count; i++)
+		{
+			if (begin[i].key > begin[i + 1].key)
+			{
+				save = begin[i];
+				begin[i] = begin[i + 1];
+				begin[i + 1] = save;
+			}
+		}
+		count++;
+	}
+
+	return true;
 }

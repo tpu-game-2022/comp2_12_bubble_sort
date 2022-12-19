14d0345a8315a00b074027eedce9b2d23d9a6551
Shun Furusawa <furu.shun1113@gmail.com>
2022-12-19 13:23:34 +0900

    修正

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -1,9 +1,8 @@
 ﻿#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
-#include "Windows.h"                    // Windows API の機能定義
+#include "Windows.h"                    
 
 #include "../include/lib_func.h"
 
-// バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
 	if (begin == NULL || end == NULL)

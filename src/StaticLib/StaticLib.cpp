42eb40bc4ffcff3f84bd2324214eb6643d83d8ba
Ton <g2227047@st.t-kougei.ac.jp>
2022-12-19 13:21:03 +0900

    テストを4つ通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,27 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (!begin || !end ||  end < begin)
+	{
+		return false;
+	}
+	
+	int save_key;
+	char save_value[256];
+	for (int i = 0; i < end->key - 1; i++)
+	{
+		for (int j = end->key - 2; j > i;j--)
+		{
+			if (begin[j].key > begin[j + 1].key)
+			{
+				save_key = begin[j].key;
+				strcpy_s(save_value, 256, begin[j].value);
+				begin[j].key = begin[j + 1].key;
+				strcpy_s(begin[j].value, 256, begin[j + 1].value);
+				begin[j + 1].key = save_key;
+				strcpy_s(begin[j + 1].value, 256, save_value);
+			}
+		}
+	}
+	return true;
 }
